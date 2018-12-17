951ea80d3eb65d7bc24af97526976a1f45d9c3cf
Jirayus <lnwnackza.jp@gmail.com>
2018-12-18 01:23:25 +0700

    add

---
+++ final-task1-master/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var db = require('./data');
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Add routing
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+var output = {
+    status: 'success',
+    message: 'REST API is working'
+}
+app.get('/api/json',function(req,res){
+    res.status(500).json(output);
+});
+
+app.get('/api/orders/', db.getOrdersAll);
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
